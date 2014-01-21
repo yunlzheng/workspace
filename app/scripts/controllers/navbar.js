b4af7f014041c000838efb5c005bfd1dcf893941
yunlzheng <yunl.zheng@gmail.com>
2014-01-21 21:39:32 +0800

    update navvar

--- app/scripts/controllers/navbar.js
+++ app/scripts/controllers/navbar.js
@@ -3,8 +3,27 @@
 angular.module('towerApp')
   .controller('NavbarCtrl', function ($scope, $location) {
     $scope.menu = [{
-      'title': 'Home',
+      'title': '项目',
       'link': '/'
+    },
+    {
+      'title': '回顾',
+      'link': '/#progress'
+    },
+    {
+      'title': '日历',
+      'link': '/#calendars'
+    },
+    {
+      'title': '团队',
+      'link': '/#members'
+    },
+    {
+      'title': '统计',
+      'link': '/#stats'
+    },{
+      'title': '我自己',
+      'link': '/#me'
     }];
     
     $scope.isActive = function(route) {
