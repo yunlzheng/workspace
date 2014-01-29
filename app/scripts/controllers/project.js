de44925e9bbed78af8348a07bf4dd9c5219a35e5
zhengyl <zhengyl@yun70.com.cn>
2014-01-29 21:43:04 +0800

    page load data from api

--- app/scripts/controllers/project.js
+++ app/scripts/controllers/project.js
@@ -2,6 +2,10 @@
 
 angular.module('towerApp')
   .controller('ProjectCtrl', function ($scope, $routeParams, $http) {
-    
+    	
+    	console.log($routeParams.project);
+    	$http.get('/api/projects/'+$routeParams.project).success(function(data){
+    		console.log(data);
+    	});
 
   });
