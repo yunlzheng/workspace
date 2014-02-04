d0f55bbffb7432eedac713dd5fe6c68107e0bb5f
zhengyl <zhengyl@yun70.com.cn>
2014-02-05 00:39:26 +0800

    消息详情页面

--- app/scripts/controllers/message.js
+++ app/scripts/controllers/message.js
@@ -2,7 +2,10 @@
 
 angular.module('towerApp')
   .controller('MessageCtrl', ['$scope', '$routeParams', '$http', function ($scope, $routeParams, $http) {
-      
-     
+      $scope.project = $routeParams.project;
+      $scope.message = $routeParams.message;
+      $http.get('/api/projects/'+$routeParams.project).success(function(data){
+    	$scope.project = data;
+      });
 
   }]);
