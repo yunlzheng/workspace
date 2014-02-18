4cc0bef9685d0bcdf273b329446fce7fff64411e
yunlzheng <yunl.zheng@gmail.com>
2014-02-18 05:40:48 +0000

    修改代码

--- app/scripts/controllers/project-setting.js
+++ app/scripts/controllers/project-setting.js
@@ -4,20 +4,13 @@ angular.module('towerApp')
   .controller('ProjectSettingCtrl', ['$scope', '$routeParams', '$http', '$modal', function ($scope, $routeParams, $http, $modal) {
     
     $scope.project_id = $routeParams.project;
-    $http.delete('/api/projects/'+$routeParams.project).success(function(data){
-          // $scope.project = data;
-          // $scope.project.name = "Hello World";
-
-          // $http.put('/api/projects/'+$routeParams.project, $scope.project).success(function(data){
-          // //$scope.project = data;
-          // });
-
+    $http.get('/api/projects/'+$routeParams.project).success(function(data){
+          $scope.project = data;
     });
-
     
-    // $http.delete('/api/projects/'+$routeParams.project).success(function(data){
-    //       $scope.project = data;
-    // });
+    //$http.put('/api/projects/'+$routeParams.project, $scope.project)
+    //$http.delete('/api/projects/'+$routeParams.project)
+   
 
   }])
   .controller('DeleteProjectModelCtrl', [function(){
